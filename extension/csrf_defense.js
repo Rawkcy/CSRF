d4efd791145feda45482003a30c51609655369e9
Roxanne Guo <roxanne.guo@me.com>
2014-07-27 12:54:56 -0400

    extension inserts session token into Faycebook form

---
+++ extension/csrf_defense.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+console.log('CSRF Nawt is running!');
+var rand = function() { return Math.random().toString(36).substr(2); }; // remove `0.`
+var generate_token = function() { return rand() + rand(); }; // to make it longer
+console.log("Generating session token ...");
+var token = generate_token();
+console.log(token);
+
+var token_field = document.createElement("input");
+token_field.setAttribute("type", "hidden");
+token_field.setAttribute("name", "__sessionToken");
+token_field.setAttribute("value", token);
+var forms = document.getElementsByTagName("form");
+for (var i=0; i<forms.length; i++) {
+  forms[i].appendChild(token_field);
+}
+
+console.log("Session token inserted.");
+console.log(forms);
